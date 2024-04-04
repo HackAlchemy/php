e58ecee76a35280e78b0aa053da04205120fafcd
alhammanazil <alhammanazil18@gmail.com>
2024-04-04 09:42:33 +0700

    continue

--- ForLoop.php
+++ ForLoop.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 for ($counter = 1; $counter <= 10; $counter++){
     echo "Ini adalah for loop ke-$counter" . PHP_EOL;
 }
@@ -16,12 +15,8 @@ for ($counter = 10; $counter >= 1; $counter--):
     echo "Ini adalah for loop ke-$counter" . PHP_EOL;
 endfor;
 
-
-=======
-
 for ($counter = 1; $counter <= 10 ;
 ) {
     echo "This is Loop $counter" . PHP_EOL;
     $counter++;
 }
->>>>>>> master
